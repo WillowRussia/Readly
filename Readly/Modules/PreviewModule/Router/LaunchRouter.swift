89578b015174c638b87c8ddd0056ee2a938200c8
WillowRussia <iv040606@gmail.com>
2025-10-03 17:52:47 +0300

    Deleated Builder

--- Readly/Modules/PreviewModule/Router/LaunchRouter.swift
+++ Readly/Modules/PreviewModule/Router/LaunchRouter.swift
@@ -28,26 +28,22 @@ final class LaunchRouterImplementation: LaunchRouter {
         
         switch state {
         case .regist:
-            let registView = Builder.createRegistView()
-//            let registView = RegistView()
-//            let configurator = RegistConfiguratorImplementation()
-//            configurator.configure(viewController: registView)
+            let registView = RegistView()
+            let configurator = RegistConfiguratorImplementation()
+            configurator.configure(viewController: registView)
             nextViewController = registView
             
         case .onboarding:
-            let onboardingView = Builder.createOnboardingView()
-//            let onboardingView = OnboardingView()
-//            let configurator = OnboardingConfiguratorImplementation()
-//            configurator.configure(viewController: onboardingView)
+            let onboardingView = OnboardingView()
+            let configurator = OnboardingConfiguratorImplementation()
+            configurator.configure(viewController: onboardingView)
             nextViewController = onboardingView
             
         case .main:
-            let mainView = Builder.createMainView()
-//            let mainView = MainView()
-//            let configurator = MainConfiguratorImplementation()
-//            configurator.configure(viewController: mainView)
-//            nextViewController = UINavigationController(rootViewController: mainView)
-            nextViewController = mainView
+            let mainView = MainView()
+            let configurator = MainConfiguratorImplementation()
+            configurator.configure(viewController: mainView)
+            nextViewController = UINavigationController(rootViewController: mainView)
         }
         
         window.rootViewController = nextViewController
